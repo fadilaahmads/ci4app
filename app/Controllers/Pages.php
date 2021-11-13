6613f99f7bae54ef55e1bdf2a9510a3562fe462c
fadilaahmads <fadilahmad59@gmail.com>
2021-11-13 16:11:46 +0700

    Add  variable contain array for title to be send in header.php

--- app/Controllers/Pages.php
+++ app/Controllers/Pages.php
@@ -6,15 +6,21 @@ class Pages extends BaseController
 {
     public function index()
     {
-        echo view('layout/header');
+        $data = [
+            'title' => 'Home | WPU'
+        ];
+        echo view('layout/header', $data); //data sent to header.php
         echo view('pages/home');
         echo view('layout/footer');
         //return view("pages/home");
     }
     public function about()
     {
+        $data = [
+            'title' => 'About Me'
+        ];
         //Calling few views
-        echo view('layout/header');
+        echo view('layout/header', $data);
         echo view('pages/about');
         echo view('layout/footer');
         //return view('pages/about');
