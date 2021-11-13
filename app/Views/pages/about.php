7b67b674d1b126af884b7c8b8995cad500a5593b
fadilaahmads <fadilahmad59@gmail.com>
2021-11-13 15:59:47 +0700

    Move content into inside container tag

--- app/Views/pages/about.php
+++ app/Views/pages/about.php
@@ -1,5 +1,12 @@
-<h1>About Me</h1>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-    Maiores voluptatum veniam nostrum id!
-    Cum dignissimos nemo illum cupiditate provident doloremque aliquam asperiores
-    soluta delectus magni voluptate dolore odio, iusto voluptatibus.</p>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1>About Me</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Maiores voluptatum veniam nostrum id!
+                Cum dignissimos nemo illum cupiditate provident doloremque aliquam asperiores
+                soluta delectus magni voluptate dolore odio, iusto voluptatibus.
+            </p>
+        </div>
+    </div>
+</div>
