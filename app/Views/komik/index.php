8f35ebd510f0c669add191d8440e94cdc945bbf4
fadilaahmads <fadilahmad59@gmail.com>
2021-11-15 22:23:36 +0700

    Add template, section, and html container

---
+++ app/Views/komik/index.php
@@ -0,0 +1,11 @@
+<?= $this->extend('layout/template'); ?>
+
+<?= $this->secteion('content'); ?>
+<div class="container">
+    <div class="row">
+        <div class="col">
+
+        </div>
+    </div>
+</div>
+<?= $this->endSection(); ?>
