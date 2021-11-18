8441dfe30882e8710595d2c629bfc2101873e76e
fadilaahmads <fadilahmad59@gmail.com>
2021-11-18 21:41:51 +0700

    Add templating, section, and html image source

--- app/Views/komik/detail.php
+++ app/Views/komik/detail.php
@@ -1,10 +1,14 @@
+<?= $this->extend('layout/template'); ?>
+
+<?= $this->section('content'); ?>
 <div class="container">
     <div class="row">
         <div class="col">
+            <h2 class="mt-2">Detail Komik</h2>
             <div class="card mb-3" style="max-width: 540px;">
                 <div class="row g-0">
                     <div class="col-md-4">
-                        <img src="..." class="img-fluid rounded-start" alt="...">
+                        <img src="/img/<?= $komik['sampul']; ?>" class="img-fluid rounded-start" alt="...">
                     </div>
                     <div class="col-md-8">
                         <div class="card-body">
@@ -18,3 +22,4 @@
         </div>
     </div>
 </div>
+<?= $this->endSection(); ?>
