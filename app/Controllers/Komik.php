663051d6f754922374afe07642c18ca58ee2eab1
fadilaahmads <fadilahmad59@gmail.com>
2021-11-18 17:19:35 +0700

    Connect to db using model

--- app/Controllers/Komik.php
+++ app/Controllers/Komik.php
@@ -2,6 +2,8 @@
 
 namespace App\Controllers;
 
+use App\Models\KomikModel;
+
 class Komik extends BaseController
 {
     public function index()
@@ -16,6 +18,9 @@ class Komik extends BaseController
         // foreach ($komik->getResultArray() as $row) {
         //     d($row);
         // }
+
+        // $komikModel = new \App\Models\KomikModel();
+        $komikModel = new KomikModel();
 
         return view('komik/index', $data);
     }
