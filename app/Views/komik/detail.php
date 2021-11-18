2c17999ba8858b029b66d85516f3322a4b9f7c1a
fadilaahmads <fadilahmad59@gmail.com>
2021-11-18 21:47:54 +0700

    Change the html card title, Add penulis and penerbit

--- app/Views/komik/detail.php
+++ app/Views/komik/detail.php
@@ -12,9 +12,9 @@
                     </div>
                     <div class="col-md-8">
                         <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                            <h5 class="card-title"><?= $komik['judul']; ?></h5>
+                            <p class="card-text"><b>Penulis: </b><?= $komik['penulis']; ?></p>
+                            <p class="card-text"><small class="text-muted"><b>Penerbit: </b><?= $komik['penerbit']; ?></small></p>
                         </div>
                     </div>
                 </div>
