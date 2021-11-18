3e728e7ad2671fa6e8b4130c5bb7f94c6316e9e4
fadilaahmads <fadilahmad59@gmail.com>
2021-11-18 17:16:56 +0700

    Add method how to connect db without model

--- app/Controllers/Komik.php
+++ app/Controllers/Komik.php
@@ -9,6 +9,14 @@ class Komik extends BaseController
         $data = [
             'title' => 'Daftar Komik'
         ];
+
+        // Cara konek ke db tanpa model
+        // $db = \Config\Database::connect();
+        // $komik = $db->query("SELECT * FROM komik");
+        // foreach ($komik->getResultArray() as $row) {
+        //     d($row);
+        // }
+
         return view('komik/index', $data);
     }
 }
