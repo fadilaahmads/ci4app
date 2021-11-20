640efaeaf31498025836a6a4d61fe882bc4e4da6
fadilaahmads <fadilahmad59@gmail.com>
2021-11-20 21:59:59 +0700

    Change column width to 8, Add div section and h2 for text Form Tambah Data Komik

--- app/Views/komik/create.php
+++ app/Views/komik/create.php
@@ -3,8 +3,10 @@
 <?= $this->section('content'); ?>
 <div class="container">
     <div class="row">
-        <div class="col">
-
+        <div class="col-8">
+            <div class="div">
+                <h2>Form Tambah Data Komik</h2>
+            </div>
         </div>
     </div>
 </div>
