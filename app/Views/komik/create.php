bdca646c648994b26c9d05a467d937b70afbba46
fadilaahmads <fadilahmad59@gmail.com>
2021-11-22 22:42:14 +0700

    Add CSRF function

--- app/Views/komik/create.php
+++ app/Views/komik/create.php
@@ -6,6 +6,7 @@
         <div class="col-8">
             <h2 class="my-3">Form Tambah Data Komik</h2>
             <form action="/komik/save" method="POST">
+                <?= csrf_field(); ?>
                 <div class="row mb-3">
                     <label for="judul" class="col-sm-2 col-form-label">Judul</label>
                     <div class="col-sm-10">
