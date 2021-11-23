6d97aa49245a230f0cf473ec4189fe007712fcc7
fadilaahmads <fadilahmad59@gmail.com>
2021-11-23 22:14:05 +0700

    Change value each input to display komik data

--- app/Views/komik/edit.php
+++ app/Views/komik/edit.php
@@ -10,7 +10,7 @@
                 <div class="row mb-3">
                     <label for="judul" class="col-sm-2 col-form-label">Judul</label>
                     <div class="col-sm-10">
-                        <input type="text" class="form-control <?= ($validation->hasError('judul') ? 'is-invalid' : ''); ?>" id="judul" name="judul" autofocus value="<?= old('judul'); ?>">
+                        <input type="text" class="form-control <?= ($validation->hasError('judul') ? 'is-invalid' : ''); ?>" id="judul" name="judul" autofocus value="<?= $komik['judul']; ?>">
                         <div class="invalid-feedback">
                             <?= $validation->getError('judul'); ?>
                         </div>
@@ -19,19 +19,19 @@
                 <div class="row mb-3">
                     <label for="penulis" class="col-sm-2 col-form-label">Penulis</label>
                     <div class="col-sm-10">
-                        <input type="text" class="form-control" id="penulis" name="penulis" value="<?= old('penulis'); ?>">
+                        <input type="text" class="form-control" id="penulis" name="penulis" value="<?= $komik['penulis']; ?>">
                     </div>
                 </div>
                 <div class="row mb-3">
                     <label for="penerbit" class="col-sm-2 col-form-label">Penerbit</label>
                     <div class="col-sm-10">
-                        <input type="text" class="form-control" id="penerbit" name="penerbit" value="<?= old('penulis'); ?>">
+                        <input type="text" class="form-control" id="penerbit" name="penerbit" value="<?= $komik['penerbit']; ?>">
                     </div>
                 </div>
                 <div class="row mb-3">
                     <label for="sampul" class="col-sm-2 col-form-label">Sampul</label>
                     <div class="col-sm-10">
-                        <input type="text" class="form-control" id="sampul" name="sampul" value="<?= old('sampul'); ?>">
+                        <input type="text" class="form-control" id="sampul" name="sampul" value="<?= $komik['sampul']; ?>">
                     </div>
                 </div>
                 <button type="submit" class="btn btn-primary">Ubah Data</button>
