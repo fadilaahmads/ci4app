31610df266ecdd8e50c959ce71c8f468d5e1e99e
fadilaahmads <fadilahmad59@gmail.com>
2021-11-23 21:34:40 +0700

    Create new route for delete komik function

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -33,7 +33,7 @@ $routes->setAutoRoute(true);
 // route since we don't have to scan directories.
 $routes->get('/', 'Pages::index');
 $routes->get('/komik/create', 'Komik::create');
-
+$routes->delete('/komik/(:num)', 'Komik::delete/$1');
 $routes->get('/komik/(:segment)', 'Komik::detail/$1');
 
 /*
