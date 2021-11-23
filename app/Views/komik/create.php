d44026f738f9476d21c5c73cc92fb846a79b09b4
fadilaahmads <fadilahmad59@gmail.com>
2021-11-23 11:29:04 +0700

    Print validation variable and get all displayed errors

--- app/Views/komik/create.php
+++ app/Views/komik/create.php
@@ -5,6 +5,7 @@
     <div class="row">
         <div class="col-8">
             <h2 class="my-3">Form Tambah Data Komik</h2>
+            <?= $validation->listErrors(); ?>
             <form action="/komik/save" method="POST">
                 <?= csrf_field(); ?>
                 <div class="row mb-3">
