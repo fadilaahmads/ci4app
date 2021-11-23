48a04b21af5be67c9008c3c47d23dc1bbcb5d8b9
fadilaahmads <fadilahmad59@gmail.com>
2021-11-23 09:43:51 +0700

    Change Tambah Data Komik button css class to mt-3

--- app/Views/komik/index.php
+++ app/Views/komik/index.php
@@ -4,8 +4,8 @@
 <div class="container">
     <div class="row">
         <div class="col">
+            <a href="/komik/create" class="btn btn-primary mt-3">Tambah Data Komik</a>
             <h1 class="mt-2">Daftar Komik</h1>
-            <a href="/komik/create" class="btn btn-primary mb-3">Tambah Data Komik</a>
             <table class="table">
                 <thead>
                     <tr>
