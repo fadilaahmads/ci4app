35480b54fccfd68bc8e92391f644def11e2beb38
fadilaahmads <fadilahmad59@gmail.com>
2021-11-23 11:44:43 +0700

    Add invalid-feedback if invalid input occur and display the error

--- app/Views/komik/create.php
+++ app/Views/komik/create.php
@@ -11,6 +11,9 @@
                     <label for="judul" class="col-sm-2 col-form-label">Judul</label>
                     <div class="col-sm-10">
                         <input type="text" class="form-control <?= ($validation->hasError('judul') ? 'is-invalid' : ''); ?>" id="judul" name="judul" autofocus>
+                        <div class="invalid-feedback">
+                            <?= $validation->getError('judul'); ?>
+                        </div>
                     </div>
                 </div>
                 <div class="row mb-3">
