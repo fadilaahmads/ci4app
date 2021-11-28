24bf8677acfe98f585b5887afc93fd4dffabbc4b
fadilaahmads <fadilahmad59@gmail.com>
2021-11-28 22:47:31 +0700

    Add hidden input to store old sampul name

--- app/Views/komik/edit.php
+++ app/Views/komik/edit.php
@@ -8,6 +8,7 @@
             <form action="/komik/update/<?= $komik['id']; ?>" method="POST" enctype="multipart/form-data">
                 <?= csrf_field(); ?>
                 <input type="hidden" name="slug" value="<?= $komik['slug']; ?>">
+                <input type="hidden" name="sampulLama" value="<?= $komik['sampul']; ?>">
                 <div class="row mb-3">
                     <label for="judul" class="col-sm-2 col-form-label">Judul</label>
                     <div class="col-sm-10">
