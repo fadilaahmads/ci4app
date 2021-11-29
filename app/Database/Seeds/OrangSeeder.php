0fe0fba90bc40c7cd18cafd90d6380be32aa2bb0
fadilaahmads <fadilahmad59@gmail.com>
2021-11-29 17:34:04 +0700

    Create: 1. Faker instance 2. Faker data comment out insert batch, use single insert

--- app/Database/Seeds/OrangSeeder.php
+++ app/Database/Seeds/OrangSeeder.php
@@ -11,25 +11,35 @@ class OrangSeeder extends Seeder
     public function run()
     {
         $timeWIB = Time::now('Asia/Jakarta');
+        // $data = [
+        //     [
+        //         'nama'          => 'Dil',
+        //         'alamat'        => 'Jl. ABCD no.99',
+        //         'created_at'    => $timeWIB,
+        //         'updated_at'    => $timeWIB
+        //     ],
+        //     [
+        //         'nama'          => 'Dal',
+        //         'alamat'        => 'Jl. ABCD no.01',
+        //         'created_at'    => $timeWIB,
+        //         'updated_at'    => $timeWIB
+        //     ],
+        //     [
+        //         'nama'          => 'Dul',
+        //         'alamat'        => 'Jl. ABCD no.02',
+        //         'created_at'    => $timeWIB,
+        //         'updated_at'    => $timeWIB
+        //     ]
+        // ];
+
+        $faker = \Faker\Factory::create();
+
         $data = [
-            [
-                'nama'          => 'Dil',
-                'alamat'        => 'Jl. ABCD no.99',
-                'created_at'    => $timeWIB,
-                'updated_at'    => $timeWIB
-            ],
-            [
-                'nama'          => 'Dal',
-                'alamat'        => 'Jl. ABCD no.01',
-                'created_at'    => $timeWIB,
-                'updated_at'    => $timeWIB
-            ],
-            [
-                'nama'          => 'Dul',
-                'alamat'        => 'Jl. ABCD no.02',
-                'created_at'    => $timeWIB,
-                'updated_at'    => $timeWIB
-            ]
+            'nama'          => $faker->name,
+            'alamat'        => $faker->address,
+            'created_at'    => $timeWIB,
+            'updated_at'    => $timeWIB
+
         ];
 
         // Simple Queries
@@ -37,7 +47,7 @@ class OrangSeeder extends Seeder
         // VALUES(:nama:, :alamat:, :created_at:, :updated_at:)", $data);
 
         // Using Query Builder
-        // $this->db->table('orang')->insert($data);
-        $this->db->table('orang')->insertBatch($data);
+        $this->db->table('orang')->insert($data);
+        // $this->db->table('orang')->insertBatch($data);
     }
 }
