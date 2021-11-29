d012029f9e9d3be6a112b6b8d367e2670b21495e
fadilaahmads <fadilahmad59@gmail.com>
2021-11-29 22:58:47 +0700

    Fix - Current page not active. Add space between two attribute class

--- app/Views/pagers/orang_pagination.php
+++ app/Views/pagers/orang_pagination.php
@@ -16,7 +16,7 @@
         <?php endif ?>
 
         <?php foreach ($pager->links() as $link) : ?>
-            <li class="page-item<?= $link['active'] ? 'active' : '' ?>">
+            <li class="page-item <?= $link['active'] ? 'active' : '' ?>">
                 <a class="page-link" href="<?= $link['uri'] ?>">
                     <?= $link['title'] ?>
                 </a>
