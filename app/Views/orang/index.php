2f618c59454516adb9746f90f88b75ef05cab19b
fadilaahmads <fadilahmad59@gmail.com>
2021-11-29 20:18:43 +0700

    Create view for orang

---
+++ app/Views/orang/index.php
@@ -0,0 +1,32 @@
+<?= $this->extend('layout/template'); ?>
+
+<?= $this->section('content'); ?>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1 class="mt-2">Daftar Orang</h1>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Nama</th>
+                        <th scope="col">Alamat</th>
+                        <th scope="col">Aksi</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <<?= $i = 1; ?> <?php foreach ($orang as $o) : ?> <tr>
+                        <th scope="row"><?= $i++; ?></th>
+                        <td><?= $o['nama']; ?></td>
+                        <td><?= $o['alamat']; ?></td>
+                        <td>
+                            <a href="" class="btn btn-success">Detail</a>
+                        </td>
+                        </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+<?= $this->endSection(); ?>
