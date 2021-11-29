3cc660894394afbc853daf65d8e4b9da0bf585b1
fadilaahmads <fadilahmad59@gmail.com>
2021-11-29 17:50:22 +0700

    Add loop for seeder/faker, use single insert, comment out insert batch

--- app/Database/Seeds/OrangSeeder.php
+++ app/Database/Seeds/OrangSeeder.php
@@ -33,21 +33,22 @@ class OrangSeeder extends Seeder
         // ];
 
         $faker = \Faker\Factory::create('id_ID');
+        for ($i = 0; $i < 100; $i++) {
+            $data = [
+                'nama'          => $faker->name,
+                'alamat'        => $faker->address,
+                'created_at'    => Time::createFromTimestamp($faker->unixTime()),
+                'updated_at'    => $timeWIB
 
-        $data = [
-            'nama'          => $faker->name,
-            'alamat'        => $faker->address,
-            'created_at'    => Time::createFromTimestamp($faker->unixTime()),
-            'updated_at'    => $timeWIB
-
-        ];
+            ];
+            // Using Query Builder
+            $this->db->table('orang')->insert($data);
+        }
 
         // Simple Queries
         // $this->db->query("INSERT INTO orang (nama, alamat, created_at, updated_at) 
         // VALUES(:nama:, :alamat:, :created_at:, :updated_at:)", $data);
 
-        // Using Query Builder
-        $this->db->table('orang')->insert($data);
         // $this->db->table('orang')->insertBatch($data);
     }
 }
