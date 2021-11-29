c4fb5cde46144325745a5d7843cb2dac3a987783
fadilaahmads <fadilahmad59@gmail.com>
2021-11-29 11:55:50 +0700

    Create Seeder file: OrangSeeder

---
+++ app/Database/Seeds/OrangSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+
+class OrangSeeder extends Seeder
+{
+    public function run()
+    {
+        $data = [
+            'nama' => 'dil',
+            'alamat'    => 'jl. abc'
+        ];
+
+        // Simple Queries
+        $this->db->query("INSERT INTO users (username, email) VALUES(:username:, :email:)", $data);
+
+        // Using Query Builder
+        $this->db->table('users')->insert($data);
+    }
+}
